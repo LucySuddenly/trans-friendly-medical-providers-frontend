57de49495056d5ad8cdc66843b8236d73f3863e1
Lucy Suddenly <suddenlylucy@gmail.com>
2019-11-25 10:13:59 -0800

    configured google maps api key env var

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function(/* env */) {
   return {
-    clientAllowedKeys: [],
+    clientAllowedKeys: ['GOOGLE_MAPS_API_KEY'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env')
