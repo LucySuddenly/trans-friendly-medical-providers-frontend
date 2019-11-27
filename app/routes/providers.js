ed90c2f9749d2c376fd79725470767e7f9a3fed0
Lucy Suddenly <suddenlylucy@gmail.com>
2019-11-27 14:26:44 -0800

    added code suggested by https://github.com/sukima

--- app/routes/providers.js
+++ app/routes/providers.js
@@ -1,18 +1,58 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+const DECODE_IN_SEQUENCE = true;
+
 export default Route.extend({
-    geocoder: service(),
-        model(){
-        return fetch('http://localhost:3000/providers')
-        .then(resp => resp.json())
-        .then(json => {
-            const geocoder = this.get('geocoder')
-            return json.map( async provider => {
-                provider.latlon = await geocoder.decode(provider.address1 + " " + provider.address2 + " " + provider.city + " " + provider.state)
-                console.log(provider.latlon)
-                return provider
-            });
-        })
+  geocoder: service(),
+
+  async model() {
+    return DECODE_IN_SEQUENCE
+      ? this.sequencedModelHook(...arguments)
+      : this.parrallelModelHook(...arguments);
+  },
+
+  async parrallelModelHook() {
+    let json = await fetch('http://localhost:3000/providers').then(r => r.json());
+    let decodes = json.map(async provider => {
+      let { address1, address2, city, state } = provider;
+      let geocodeAddress = [address1, address2, city, state].join(' ');
+      let latlon = await this.geocoder.decode(geocodeAddress);
+      return { ...provider, latlon };
+    });
+    return await Promise.all(decodes);
+  },
+
+  async sequencedModelHook() {
+    let providers = [];
+    let json = await fetch('http://localhost:3000/providers').then(r => r.json());
+    for (let provider of json) {
+      let { address1, address2, city, state } = provider;
+      let geocodeAddress = [address1, address2, city, state].join(' ');
+      let latlon = await this.geocoder.decode(geocodeAddress);
+      providers.push({ ...provider, latlon });
     }
+    return providers;
+  }
+
 });
+
+
+// import Route from '@ember/routing/route';
+// import { inject as service } from '@ember/service';
+
+// export default Route.extend({
+//     geocoder: service(),
+//         model(){
+//         return fetch('http://localhost:3000/providers')
+//         .then(resp => resp.json())
+//         .then(json => {
+//             const geocoder = this.get('geocoder')
+//             return json.map( async provider => {
+//                 provider.latlon = await geocoder.decode(provider.address1 + " " + provider.address2 + " " + provider.city + " " + provider.state)
+//                 console.log(provider.latlon)
+//                 return provider
+//             });
+//         })
+//     }
+// });
--- app/services/geocoder.js
+++ app/services/geocoder.js
@@ -1,23 +1,54 @@
-import Service, {inject as service} from '@ember/service';
-import { reads } from '@ember/object/computed'
-import { computedPromise } from 'ember-google-maps/utils/helpers';
+import Service, { inject as service } from '@ember/service';
+import { get } from '@ember/object';
+
+const NULL_LOCATION = {
+  lat() {
+    return null;
+  },
+  lng() {
+    return null;
+  }
+};
 
 export default Service.extend({
-    googleMapsApi: service(),
-    google: reads('googleMapsApi.google'),
-    geocoder: computedPromise(function() {
-        return this.get('google').then((google) => new google.maps.Geocoder());
-      }),
-    init(){
-        this._super(...arguments)
-    },
-    async decode(address){
-        return await this.get('geocoder').then(geocoder => geocoder.geocode({address: address}, function(results, status){
-            if (status == 'OK'){
-                const latlon = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
-                console.log(latlon)
-                return latlon
-            }
-        }))
+  googleMapsApi: service(),
+
+  async decode(address) {
+    const google = await this.googleMapsApi.google;
+    let geocoder = new google.maps.Geocoder();
+    let [results, status] = await new Promise(resolve => {
+      geocoder.geocode({ address }, (results, status) => resolve([results, status]));
+    });
+    if (status !== 'OK') {
+      let error = new Error(`Geocode resonse had status '${status}'`);
+      error.code = 'EGEOCODENOTOK';
+      throw error;
     }
+    let location = get(results, 'firstObject.geometry.location') || NULL_LOCATION;
+    return [location.lat(), location.lng()];
+  }
 });
+
+// import Service, {inject as service} from '@ember/service';
+// import { reads } from '@ember/object/computed'
+// import { computedPromise } from 'ember-google-maps/utils/helpers';
+
+// export default Service.extend({
+//     googleMapsApi: service(),
+//     google: reads('googleMapsApi.google'),
+//     geocoder: computedPromise(function() {
+//         return this.get('google').then((google) => new google.maps.Geocoder());
+//       }),
+//     init(){
+//         this._super(...arguments)
+//     },
+//     async decode(address){
+//         return await this.get('geocoder').then(geocoder => geocoder.geocode({address: address}, function(results, status){
+//             if (status == 'OK'){
+//                 const latlon = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
+//                 console.log(latlon)
+//                 return latlon
+//             }
+//         }))
+//     }
+// });
