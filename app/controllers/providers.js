ad539aba9408338e4f61917deab2252b50fbfdd5
Lucy Suddenly <suddenlylucy@gmail.com>
2019-12-09 08:27:47 -0800

    added save function to providers controller with post to backend

--- app/controllers/providers.js
+++ app/controllers/providers.js
@@ -2,9 +2,40 @@ import Controller from '@ember/controller';
 import { set } from '@ember/object';
 
 export default Controller.extend({
+    name: '',
+    description: '',
+    address1: '',
+    address2: '',
+    city: '',
+    state: '',
+    zip: '',
+    website: '',
+    phone: '',
     actions: {
         toggle: function(provider) {
             set(provider, 'markerOpen', !provider.markerOpen)
+        },
+        save: function() {
+            fetch('http://localhost:3000/providers',{
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                provider: {
+                    name: this.get('name'),
+                    description: this.get('description'),
+                    address1: this.get('address1'),
+                    address2: this.get('address2'),
+                    city: this.get('city'),
+                    state: this.get('state'),
+                    zip: this.get('zip'),
+                    website: this.get('website'),
+                    phone: this.get('phone')
+                }
+            })
+            })
         }
     }
 });
