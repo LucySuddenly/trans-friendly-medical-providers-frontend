84422b29c82a0cec19316eeba0a004ae49bad9ed
Lucy Suddenly <suddenlylucy@gmail.com>
2019-12-09 13:23:54 -0800

    added basic error return on unsuccessful provider creation

--- app/controllers/providers.js
+++ app/controllers/providers.js
@@ -36,6 +36,10 @@ export default Controller.extend({
                 }
             })
             })
+            .then(resp => resp.json())
+            .then(json => {
+                this.set('errorMessage', json)
+            })
         }
     }
 });
