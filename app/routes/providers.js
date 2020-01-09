ccd73beba506245a5e01c5da456af54488b9360d
Lucy Suddenly <suddenlylucy@gmail.com>
2020-01-09 14:06:55 -0800

    stripping old code

--- app/routes/providers.js
+++ app/routes/providers.js
@@ -40,21 +40,3 @@ export default Route.extend({
 });
 
 
-// import Route from '@ember/routing/route';
-// import { inject as service } from '@ember/service';
-
-// export default Route.extend({
-//     geocoder: service(),
-//         model(){
-//         return fetch('http://localhost:3000/providers')
-//         .then(resp => resp.json())
-//         .then(json => {
-//             const geocoder = this.get('geocoder')
-//             return json.map( async provider => {
-//                 provider.latlon = await geocoder.decode(provider.address1 + " " + provider.address2 + " " + provider.city + " " + provider.state)
-//                 console.log(provider.latlon)
-//                 return provider
-//             });
-//         })
-//     }
-// });
--- app/services/geocoder.js
+++ app/services/geocoder.js
@@ -28,27 +28,3 @@ export default Service.extend({
     return [location.lat(), location.lng()];
   }
 });
-
-// import Service, {inject as service} from '@ember/service';
-// import { reads } from '@ember/object/computed'
-// import { computedPromise } from 'ember-google-maps/utils/helpers';
-
-// export default Service.extend({
-//     googleMapsApi: service(),
-//     google: reads('googleMapsApi.google'),
-//     geocoder: computedPromise(function() {
-//         return this.get('google').then((google) => new google.maps.Geocoder());
-//       }),
-//     init(){
-//         this._super(...arguments)
-//     },
-//     async decode(address){
-//         return await this.get('geocoder').then(geocoder => geocoder.geocode({address: address}, function(results, status){
-//             if (status == 'OK'){
-//                 const latlon = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
-//                 console.log(latlon)
-//                 return latlon
-//             }
-//         }))
-//     }
-// });
